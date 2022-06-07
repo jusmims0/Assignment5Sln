0be5a81a16e0dbcc7f0fea4a7f91a4fa7c430ce8
jusmims0 <102726528+jusmims0@users.noreply.github.com>
2022-06-07 08:50:39 +0200

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] totalNumber = { -1, 2, 4, 6, 9 };
+            int positveNumbers = 0;
+            int negativeNumbers = 0;
+            for (int i = 0; i < totalNumber.Length; i++)
+            {
+                if (totalNumber[i] < 0)
+                {
+                    negativeNumbers++;
+                }
+                else
+                {
+                    positveNumbers++;
+                }
+            }
+            Console.WriteLine($"postive numbers:{positveNumbers}");
+            Console.WriteLine($"negative numbers:{negativeNumbers}");
+
+
+        }
+    }
+}
